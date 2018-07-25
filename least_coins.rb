f8d1367bfe25f07a46d120bb86c2bef58b644e3c
nicole03 <ntidwell03@gmail.com>
2018-07-25 18:30:33 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -3,9 +3,9 @@
 def least_coins(cents)
   least_coins2 = {} 
   least_coins2= {"quarters" => 1}
-  least_coins2 ["dimes"] = 0 
-  least_coins2 ["nickels"] = 0 
-  least_coins2 ["pennies"] = 4 
+  least_coins2["dimes"] = 0 
+  least_coins2["nickels"] = 0 
+  least_coins2["pennies"] = 4 
   puts cents 
 
 end
