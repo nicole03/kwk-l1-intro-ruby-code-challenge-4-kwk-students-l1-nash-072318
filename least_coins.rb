97a2bec74ff291cb5750d48e35f1824a39f71785
nicole03 <ntidwell03@gmail.com>
2018-07-25 18:52:00 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -4,7 +4,9 @@ def least_coins(cents)
   least_coins2 = {} 
  num_quarters = cents / 25
  least_coins2[:quarters]=num_quarters
+ num_dimes = cents / 10 
+ least_coins2[:dimes]=num_dimes
  
 puts least_coins2 
 end
-least_coins (50)
+least_coins (70)
